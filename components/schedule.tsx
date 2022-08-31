53e9ecfc2d6c0a7bfdec4cecd57ef690f61b862e
Daniil <dadyarri@gmail.com>
2022-09-01 02:31:00 +0300

    remove useless stuff

--- components/schedule.tsx
+++ components/schedule.tsx
@@ -19,27 +19,6 @@ type TimetableItem = {
 };
 
 const Schedule = ({ schedule }: Props) => {
-  const dayNumberToName = (day: number) => {
-    switch (day) {
-      case 1:
-        return "Воскресенье";
-      case 2:
-        return "Понедельник";
-      case 3:
-        return "Вторник";
-      case 4:
-        return "Среда";
-      case 5:
-        return "Четверг";
-      case 6:
-        return "Пятница";
-      case 7:
-        return "Суббота";
-      default:
-        return "";
-    }
-  };
-
   const dayNumberToShortName = (day: number) => {
     switch (day) {
       case 1:
