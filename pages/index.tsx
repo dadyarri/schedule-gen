d91fe3d9331eb51407c986785e2a43195a812bbe
Daniil <dadyarri@gmail.com>
2022-08-31 19:15:33 +0300

    add layout to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
+import Layout from "../components/layout";
+import {Heading} from "@chakra-ui/react";
 
 const Home: NextPage = () => {
-  return <></>;
+  return <Layout title={"Расписание"}>
+    <Heading as={"h2"} alignSelf={"center"}>Генератор расписания</Heading>
+  </Layout>;
 };
 
 export default Home;
