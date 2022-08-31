14bda6f9703904999e884cd89c67d5245976f45d
Daniil <dadyarri@gmail.com>
2022-08-31 19:15:21 +0300

    add container to layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import {motion} from "framer-motion";
 import Head from "next/head";
 import React from "react";
+import {Container} from "@chakra-ui/react";
 
 const variants = {
   hidden: { opacity: 0, x: 0, y: 20 },
@@ -30,7 +31,9 @@ const Layout = ({ children, title }: Props) => {
           <title>{t}</title>
       </Head>
   )}
-  {children}
+  <Container mt={5}>
+    {children}
+  </Container>
   </>
   </motion.article>
 );
