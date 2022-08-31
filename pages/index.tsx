33cbbcad78c206de497befde944eeee909fba8fd
Daniil <dadyarri@gmail.com>
2022-08-31 23:27:12 +0300

    add reading of uploaded archive, encoding json inside it to base64, and putting it to url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,159 @@
-import type { NextPage } from "next";
+import type {NextPage} from "next";
 import Layout from "../components/layout";
-import {Heading} from "@chakra-ui/react";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  Box,
+  Button,
+  CloseButton,
+  FormControl,
+  Heading,
+  Input,
+  Text,
+  useDisclosure
+} from "@chakra-ui/react";
+import {useRouter} from "next/router";
+import {AiOutlineUpload} from "react-icons/ai";
+import React, {FormEvent} from "react";
+import {BlobReader, TextWriter, ZipReader} from "@zip.js/zip.js";
 
 const Home: NextPage = () => {
-  return <Layout title={"Расписание"}>
-    <Heading as={"h2"} alignSelf={"center"}>Генератор расписания</Heading>
-  </Layout>;
+  const router = useRouter();
+  const schedule = router.query["schedule"];
+  const {
+    isOpen: isMissingFileVisible,
+    onClose: onMissingFileClose,
+    onOpen: onMissingFileOpen
+  } = useDisclosure({ defaultIsOpen: false });
+
+  const {
+    isOpen: isMoreThanOneFileVisible,
+    onClose: onMoreThanOneFileClose,
+    onOpen: onMoreThanOneFileOpen
+  } = useDisclosure({ defaultIsOpen: false });
+
+  function b64EncodeUnicode(str: string) {
+    return window.btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+        function toSolidBytes(match, p1) {
+          // @ts-ignore
+          return String.fromCharCode('0x' + p1);
+        }));
+  }
+
+  function b64DecodeUnicode(str: string) {
+    return decodeURIComponent(window.atob(str).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+  }
+
+
+  const uploadSchedule = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // read uploaded file +
+    // check if it's the zip archive with a json inside
+    // validate schema
+    // if yes, base64 the json and push it to url
+    // if no, show error
+
+    const fileInput = document.getElementById("file-input") as HTMLInputElement;
+
+    if (fileInput.files?.length === 0 || !fileInput.files) {
+      return onMissingFileOpen();
+    } else {
+      const file = fileInput.files[0] as File;
+      // @ts-ignore
+      const entries = await new ZipReader(new BlobReader(file)).getEntries({});
+
+      if (entries.length !== 1) {
+        return onMoreThanOneFileOpen();
+      }
+
+      // @ts-ignore
+      const json = await entries[0].getData(new TextWriter()).then(result => {
+        return JSON.parse(result);
+      });
+
+      const jsonString = JSON.stringify(json);
+      const jsonBase64 = b64EncodeUnicode(jsonString);
+
+      await router.push(`/?schedule=${jsonBase64}`, undefined, {
+        shallow: true
+      });
+    }
+  };
+
+  return (
+    <Layout title={"Расписание"}>
+      <Heading as={"h2"} alignSelf={"center"}>
+        Генератор расписания
+      </Heading>
+      {schedule ? (
+        <Text>{schedule}</Text>
+      ) : (
+        <Box mt={10}>
+          <form method={"post"} onSubmit={uploadSchedule}>
+            <FormControl>
+              <Input
+                type={"file"}
+                accept={".zip"}
+                size={"sm"}
+                name={"file"}
+                id={"file-input"}
+              />
+              <Button
+                leftIcon={<AiOutlineUpload />}
+                colorScheme="blue"
+                type={"submit"}
+              >
+                Открыть существующее расписание
+              </Button>
+            </FormControl>
+          </form>
+          <Text fontSize={"xs"} color={"gray.500"}>
+            Данные никуда не загружаются и обрабатываются в вашем браузере
+          </Text>
+        </Box>
+      )}
+      {isMissingFileVisible && (
+        <Alert
+          status="error"
+          mt={4}
+          id={"file-missing-error-alert"}
+          variant={"left-accent"}
+        >
+          <AlertIcon />
+          <AlertTitle>Добавьте файл!</AlertTitle>
+          <CloseButton
+            alignSelf="flex-start"
+            position="relative"
+            right={-1}
+            top={-1}
+            onClick={onMissingFileClose}
+          />
+        </Alert>
+      )}
+
+      {isMoreThanOneFileVisible && (
+        <Alert
+          status="error"
+          mt={4}
+          id={"more-than-one-file-error-alert"}
+          variant={"left-accent"}
+        >
+          <AlertIcon />
+          <AlertTitle>В архиве более одного файла!</AlertTitle>
+          <CloseButton
+            alignSelf="flex-start"
+            position="relative"
+            right={-1}
+            top={-1}
+            onClick={onMoreThanOneFileClose}
+          />
+        </Alert>
+      )}
+    </Layout>
+  );
 };
 
 export default Home;
