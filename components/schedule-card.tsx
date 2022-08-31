854144ab60a3d5f01e79c7be01dc83e0644bbf97
Daniil <dadyarri@gmail.com>
2022-09-01 02:40:16 +0300

    reformat code

--- components/schedule-card.tsx
+++ components/schedule-card.tsx
@@ -4,7 +4,9 @@ import { TimeTable } from "../libs/types";
 const ScheduleCard = ({ lesson, room, type, teacher, time }: TimeTable) => {
   return (
     <VStack border={"1px"} borderRadius={"5px"} p={2} m={2} align={"left"}>
-      <Text fontWeight={"bold"} fontSize={"20px"}>{time}</Text>
+      <Text fontWeight={"bold"} fontSize={"20px"}>
+        {time}
+      </Text>
       <Text fontWeight={"semibold"} fontSize={"19px"}>
         {lesson} ({type})
       </Text>
